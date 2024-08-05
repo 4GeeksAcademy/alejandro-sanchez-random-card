5db09e5a44d1e3b094d6fae06e81705096285208
Alejosz1902 <111547538+Alejosz1902@users.noreply.github.com>
2024-08-05 20:22:49 +0000

    Completed card generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,50 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const cards = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+const suits = [
+  { name: "♠️", icon: "text-dark fs-1" },
+  { name: "♥", icon: "text-danger fs-1" },
+  { name: "♣", icon: "text-dark fs-1" },
+  { name: "♦", icon: "text-danger fs-1" }
+];
+
+let getRandomCard = () => {
+  let card = cards[Math.floor(Math.random() * cards.length)];
+  let cardNumberText = document.querySelector("h1");
+  let suit = suits[Math.floor(Math.random() * suits.length)];
+  let suitText = document.querySelectorAll("h2");
+
+  cardNumberText.innerHTML = `${card}`;
+  cardNumberText.className = `${suit.icon}`;
+  suitText.forEach(text => {
+    text.innerHTML = `${suit.name}`;
+    text.className = `${suit.icon}`;
+  });
 };
+
+window.onload = getRandomCard();
+{
+}
+
+const button = document.getElementById("newCard");
+button.addEventListener("click", getRandomCard);
+
+const intervalo = setInterval(getRandomCard, 10000);
